156576522cb4f95092482db9c027447cee3c58f8
Adam Magaluk <AdamMagaluk@google.com>
2017-02-17 07:09:18 -0700

    Trigger updated to demo

--- trigger.js
+++ trigger.js
@@ -1,16 +1,49 @@
+var request = require('request');
 module.exports = function(server) {
-  var ledQuery = server.where({ type: 'led' });
-  server.observe([ledQuery], function(led){
+  var changeCount = 0;
+  var doorQuery = server.where({ type: 'door' });
+  server.observe([doorQuery], function(door){
+	console.log('changeCount' + changeCount)
+    console.log('found door', door.once)
 
-    console.log('found led', led.once)
-
-    led.on('turn-on', function() {
+    door.on('open', function() {
+		changeCount++;
       console.log('!!!turning on')
+	  console.log('changeCount' + changeCount)
+ if(changeCount === 2){
+		console.log('!!!call api')
+		console.log('changeCount' + changeCount)
+		changeCount=0;
+		 // api call..
+		 callPush();
+	}
     });
-    led.on('turn-off', function() {
+    door.on('close', function() {
+		changeCount++;
       console.log('!!!turning off')
-      // api call..
+	  	console.log('changeCount' + changeCount)
+ if(changeCount === 2){
+		console.log('!!!call api')
+		console.log('changeCount' + changeCount)
+		changeCount=0;
+		 // api call..
+		 callPush();
+	}
+     
     });
-    
+   
   });
+}
+
+function callPush(){
+	var requestData = {"EXTERNALID" : "demouser"}
+	request({
+    url: 'http://54.154.5.203:9008/pushnotificationiot',
+    method: "POST",
+    json: true,
+    headers: {
+        "content-type": "application/json",
+    },
+    body: requestData
+	})
 }
